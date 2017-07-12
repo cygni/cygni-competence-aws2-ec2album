975ed8707f9e0d0083611255d1b16f7ed558a6a3
Robert Buren <robert@buren.se>
2017-07-13 00:22:30 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 sudo yum update -y
 sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
-sudo yum install nodejs -y
+sudo yum install git nodejs -y
+git clone https://github.com/robu/ec2-album.git
