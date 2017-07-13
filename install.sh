256a6e322733eab7f7e8078d48802814b6f9e08a
Robert Buren <robert@buren.se>
2017-07-13 11:18:59 +0200

    added bucketname replace in install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ sudo yum update -y
 sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
 sudo yum install git nodejs -y
 git clone https://github.com/robu/ec2-album.git
+cd ec2-album
+sed -i 's/examplebucket/ACTUALBUCKETNAME/g' index.js
+sudo node index.js
