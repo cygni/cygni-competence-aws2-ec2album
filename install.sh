a1f0a339f7cf1b7ea81668da7b0b44959500b30d
Robert Buren <robert@buren.se>
2017-12-11 20:23:59 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+cd /home/ec2-user
 sudo yum update -y
 sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
 sudo yum install git nodejs -y
-git clone https://github.com/cygni/cygni-competence-aws2-ec2album
+cd /home/ec2-user
+git clone https://github.com/cygni/cygni-competence-aws2-ec2album ec2-album
 cd ec2-album
 sed -i 's/examplebucket/ACTUALBUCKETNAME/g' index.js
 npm install
