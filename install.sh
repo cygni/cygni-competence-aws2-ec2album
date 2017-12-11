b06d78794e2e79744dfaef29edab2c524d4859f9
Robert Buren <robert@buren.se>
2017-12-11 20:09:08 +0100

    corrected clone url in install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo yum update -y
 sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
 sudo yum install git nodejs -y
-git clone https://github.com/robu/ec2-album.git
+git clone https://github.com/cygni/cygni-competence-aws2-ec2album
 cd ec2-album
 sed -i 's/examplebucket/ACTUALBUCKETNAME/g' index.js
 npm install
