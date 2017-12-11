5d3d21a17a6f0eb8c9fd09376707ae06451a00c2
Robert Buren <robert@buren.se>
2017-12-11 20:30:11 +0100

    removed redundant 'cd' from install script

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ cd /home/ec2-user
 sudo yum update -y
 sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
 sudo yum install git nodejs -y
-cd /home/ec2-user
 git clone https://github.com/cygni/cygni-competence-aws2-ec2album ec2-album
 cd ec2-album
 sed -i 's/examplebucket/ACTUALBUCKETNAME/g' index.js
